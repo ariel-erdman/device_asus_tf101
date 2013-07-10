664d15aac2fc1a522ba61ee1a4dc6aa88a3b0fae
TheMrcool212 <munirmohamadrani@gmail.com>
2013-07-11 06:04:06 +0800

    Trebuchet Launcher by default and AutoParts settings testing

--- device.mk
+++ device.mk
@@ -146,7 +146,8 @@ PRODUCT_PACKAGES += \
     tinyplay \
     tinyrec \
     audio.primary.ventana \
-    AutoParts_tfp
+    AutoParts_tfp \
+    Trebuchet
 
 # override
 PRODUCT_PROPERTY_OVERRIDES := \
